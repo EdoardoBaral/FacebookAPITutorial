bf0185716a394440bcf42202d617fe1d21b2c0cf
Edoardo Baral <edoardo@baral.it>
2020-06-06 14:54:54 +0200

    Lezione 1 - Aggiunta dipendenza nel POM per libreria RestFB e classe di main

---
+++ src/main/java/lezione01/Lezione01.java
@@ -0,0 +1,9 @@
+package lezione01;
+
+public class Lezione01
+{
+	public static void main(String[] args)
+	{
+		System.out.println("La prima lezione consiste solamente nell'aggiungere al POM la dipendenza per la libreria RestFB");
+	}
+}
